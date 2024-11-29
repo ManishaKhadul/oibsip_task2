5c13561fb44a7c88402511a6d3955ed318e8227c
manisha <manishakhadul6@gmail.com>
2024-11-29 09:24:33 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Function to toggle the menu visibility on mobile
+function toggleMenu() {
+  const menu = document.querySelector('.menu');
+  menu.classList.toggle('active');  // Toggle the 'active' class to show/hide the menu
+}
